28db7b61ca2f87d78fc19de8fc623f286375019a
谭鑫 <7366974+smellh@user.noreply.gitee.com>
2021-12-20 17:48:56 +0800

    备注信息

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+
+</style>
