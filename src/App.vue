12dc1af1575285eb4287e426fe9caa23485f05d3
谭鑫 <7366974+smellh@user.noreply.gitee.com>
2021-12-20 21:03:28 +0800

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<div class="main"></div>
     <router-view/>
   </div>
 </template>
@@ -11,5 +17,10 @@ export default {
 </script>
 
 <style lang="less">
+.main{
+  width: 200px;
+  height: 100px;
+  background: green;
+}
 
 </style>
